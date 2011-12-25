9b8509f117687fec77ccc42c80a6253582354818
Ammar Khaku <ammar.khaku@gmail.com>
2011-12-25 16:11:22 -0500

    Skinned out css file, added jquery

--- tutoringapp/urls.py
+++ tutoringapp/urls.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.conf.urls.defaults import patterns, include, url
 
 # Uncomment the next two lines to enable the admin:
@@ -8,4 +9,7 @@ urlpatterns = patterns('',
         (r'^$', 'landing.views.home'),
         (r'^tutors/$', 'tutors.views.all_tutors'),
         (r'^tutors/', include('tutoringapp.tutors.urls')),
+        # Serve static assets for dev
+        (r'^static/(?P<path>.*)$', 'django.views.static.serve',
+            {'document_root': settings.MEDIA_ROOT}),
     )
