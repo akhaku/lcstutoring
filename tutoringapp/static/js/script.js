b4e38e96729ce7cc40cfab492e5a10b6ff3d663f
Ammar Khaku <ammar.khaku@gmail.com>
2012-01-05 18:52:46 -0500

    Draggable and droppable tutors and tutees for matches

--- tutoringapp/static/js/script.js
+++ tutoringapp/static/js/script.js
@@ -1,4 +1,22 @@
 $(document).ready(function() {
-    $('.tutor-cand').draggable();
-    $('.tutee-cand').draggable();
+    $('.tutor-cand, .tutee-cand').draggable({
+        helper: function(event) {
+                    console.log($(event.target));
+                return $(event.target).clone();
+            }
+    });
+    $('#match-box').droppable({
+        accept: '.tutor-cand, .tutee-cand',
+        tolerance: 'pointer',
+        drop: function(event, ui) {
+            if(ui.helper.hasClass('tutee-cand')) {
+                which = "tutee";
+            } else if(ui.helper.hasClass('tutor-cand')) {
+                which = "tutor";
+            }
+            console.log(ui.helper.html());
+            $('span#'+which+'-selected').text(ui.helper.html());
+            $('span#'+which+'-selected').attr('rel',ui.helper.attr('rel'));
+        }
+    });
 });
