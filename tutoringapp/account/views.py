539ae9ea0f7f6dbfb824b5a8f7622b195ddf562e
Ammar Khaku <ammar.khaku@gmail.com>
2012-01-10 04:19:29 -0500

    Next parameter working on login

--- tutoringapp/account/views.py
+++ tutoringapp/account/views.py
@@ -4,6 +4,7 @@ from django.core.urlresolvers import reverse
 from django.http import HttpResponseRedirect
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+from urllib import urlencode
 
 def home_page(request):
     if not request.user.is_authenticated():
@@ -14,19 +15,22 @@ def login_page(request):
     if request.user.is_authenticated():
         return HttpResponseRedirect(reverse('account.views.home_page',
                 args=[]))
-    return render_to_response('login.html', {},
-            context_instance=RequestContext(request))
+    return render_to_response('login.html', { 'redirect_to': request.GET.get('next')},
+        context_instance=RequestContext(request))
 
 def auth(request):
     username = request.REQUEST.get('username')
     password = request.REQUEST.get('password')
     user = authenticate(username=username, password=password)
+    redirect_to = request.REQUEST.get('next')
     if user is not None:
         messages.info(request, "Logged in as %s" % username)
         login(request, user)
+        return HttpResponseRedirect(redirect_to)
     else:
         messages.error(request, "Could not log you in as %s" % username)
-    return HttpResponseRedirect(reverse('account.views.login_page',args=[]))
+    return HttpResponseRedirect("%s?%s" % (reverse('account.views.login_page',
+        args=[]), urlencode(dict(next=redirect_to)) ) )
 
 def logout_page(request):
     logout(request)
